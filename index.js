ab87d019087fb4bfd551748f11555d8ad26ac1df
scottdath <151474770+scottdath@users.noreply.github.com>
2023-11-22 09:41:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://ruwasasoft.netlify.app/',
     methods: ['POST', 'PUT', 'GET', 'OPTIONS', 'HEAD','DELETE'],
     credentials: true
   }));
