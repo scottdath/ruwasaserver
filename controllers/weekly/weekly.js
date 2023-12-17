914caed089c0d28093c045e999e1cce20adaaf45
scottdath <151474770+scottdath@users.noreply.github.com>
2023-12-18 02:58:13 +0300

    Update weekly.js

--- controllers/weekly/weekly.js
+++ controllers/weekly/weekly.js
@@ -6,7 +6,7 @@ export const getallData =(req,res)=>{
             connection.query('SELECT * FROM pmsweekly',(err,rows)=>{
                 connection.release()
 
-                console.log(rows[0])
+                console.log(rows)
                 
                 const data = {
                     'monday': (rows[0].days==='monday') ? Number(rows[0].hhc) + Number(rows[0].zonal) + Number(rows[0].public) : '0',
