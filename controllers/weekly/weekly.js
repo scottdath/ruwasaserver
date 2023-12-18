f43db84e5476125850d34d372a2006bd760bfd05
scottdath <151474770+scottdath@users.noreply.github.com>
2023-12-18 03:02:22 +0300

    Update weekly.js

--- controllers/weekly/weekly.js
+++ controllers/weekly/weekly.js
@@ -6,16 +6,16 @@ export const getallData =(req,res)=>{
             connection.query('SELECT * FROM pmsweekly',(err,rows)=>{
                 connection.release()
 
-                console.log(rows)
+                // console.log(rows)
                 
                 const data = {
                     'monday': (rows[0].days==='monday') ? Number(rows[0].hhc) + Number(rows[0].zonal) + Number(rows[0].public) : '0',
-                    'tuesday': (rows[0].days==='tuesday') ? Number(rows[0].hhc) + Number(rows[0].zonal) + Number(rows[0].public) : '0',
-                    'wednesday': (rows[0].days==='wednesday') ? Number(rows[0].hhc) + Number(rows[0].zonal) + Number(rows[0].public) : '0',
-                    'thursday': (rows[0].days==='thursday') ? Number(rows[0].hhc) + Number(rows[0].zonal) + Number(rows[0].public) : '0',
-                    'friday': (rows[0].days==='friday') ? Number(rows[0].hhc) + Number(rows[0].zonal) + Number(rows[0].public) : '0',
-                    'saturday': (rows[0].days==='saturday') ? Number(rows[0].hhc) + Number(rows[0].zonal) + Number(rows[0].public) : '0',
-                    'sunday': (rows[0].days==='sunday') ? Number(rows[0].hhc) + Number(rows[0].zonal) + Number(rows[0].public) : '0'
+                    'tuesday': (rows[1].days==='tuesday') ? Number(rows[1].hhc) + Number(rows[0].zonal) + Number(rows[1].public) : '0',
+                    'wednesday': (rows[2].days==='wednesday') ? Number(rows[2].hhc) + Number(rows[0].zonal) + Number(rows[2].public) : '0',
+                    'thursday': (rows[3].days==='thursday') ? Number(rows[3].hhc) + Number(rows[0].zonal) + Number(rows[3].public) : '0',
+                    'friday': (rows[4].days==='friday') ? Number(rows[4].hhc) + Number(rows[0].zonal) + Number(rows[4].public) : '0',
+                    'saturday': (rows[5].days==='saturday') ? Number(rows[5].hhc) + Number(rows[0].zonal) + Number(rows[5].public) : '0',
+                    'sunday': (rows[6].days==='sunday') ? Number(rows[6].hhc) + Number(rows[0].zonal) + Number(rows[6].public) : '0'
                 }
 
                 
