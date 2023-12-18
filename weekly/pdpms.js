0cf755c199a4a7ffadf5dfd1871e90a016cc66d1
scottdath <151474770+scottdath@users.noreply.github.com>
2023-12-18 13:58:13 +0300

    Update pdpms.js

--- weekly/pdpms.js
+++ weekly/pdpms.js
@@ -31,8 +31,8 @@ export const pdweeklyfetch = async() =>
                 connection.query( "UPDATE messagenos SET currentid = ?, previousid = ?  WHERE id = ?", [String(dat_len),String(dat_len),1], (err, rows) => {
                 connection.release() 
                 if (!err) {
-                     let n = 50;  // replace with num_of_messages var
-                      while(n >= 0){
+                   let n = 0;  // replace with num_of_messages var
+                      for (n = 0; n<=num_of_message; n++){
                         let result = dat[ n ]
                         let message = result.msg 
                         let recv_time = result.dt
